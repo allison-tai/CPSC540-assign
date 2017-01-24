4e9097518ceeda163e11be7f3553f8a04821912b
sakukoivu <alistair.barton@mail.mcgill.ca>
2017-01-23 22:31:45 -0800

    question 2.2, parts 1,2

--- a2/example_group.m
+++ a2/example_group.m
@@ -1,8 +1,12 @@
 clear all
 
 load groupData.mat
+lambda = 10;
 
-model = softmaxClassifier(X,y);
+%model = softmaxClassifier(X,y);
+%model = softmaxClassifierL2(X,y,lambda);
+model = softmaxClassifierL1(X,y,lambda);
+
 
 yhat = model.predict(model,X);
 errTrain = sum(yhat~=y)/length(y)
---
+++ a2/softmaxClassifierL2.m
@@ -0,0 +1,36 @@
+function [model] = softmaxClassifier(X,y,lambda)
+
+% Compute sizes
+[n,d] = size(X);
+k = max(y);
+
+W = zeros(d,k); % Each column is a classifier
+W(:) = findMin(@softmaxLoss,W(:),500,1,X,y,k,lambda);
+
+model.W = W;
+model.predict = @predict;
+end
+
+function [yhat] = predict(model,X)
+W = model.W;
+[~,yhat] = max(X*W,[],2);
+end
+
+function [nll,g,H] = softmaxLoss(w,X,y,k,lambda)
+
+[n,p] = size(X);
+W = reshape(w,[p k]);
+
+XW = X*W;
+Z = sum(exp(XW),2);
+
+ind = sub2ind([n k],[1:n]',y);
+nll = -sum(XW(ind)-log(Z)) + lambda*norm(W,'fro')^2/2;
+
+g = zeros(p,k);
+for c = 1:k
+    g(:,c) = X'*(exp(XW(:,c))./Z-(y == c));
+end
+g = g + lambda*W;
+g = reshape(g,[p*k 1]);
+end
