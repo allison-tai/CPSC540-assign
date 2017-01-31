bbedd69711c4bbc0fff3d374a7eee1f7ef8e347d
sakukoivu <alistair.barton@mail.mcgill.ca>
2017-01-31 01:28:43 -0800

    3.2 pretty sure this is final

--- a2/example_dual.m
+++ a2/example_dual.m
@@ -18,33 +18,29 @@ z = zeros(n,1);
 YX = diag(y)*X;
 G = YX*YX';
 
-% Convert from dual to primal variables
-w = (1/lambda)*(YX'*z);
-
-% Evaluate primal objective:
-P0 = sum(max(1-y.*(X*w),0)) + (lambda/2)*(w'*w);
-
-e = ones(n,1);
-% Evaluate dual objective:
-D0 = sum(z) - (z'*G*z)/(2*lambda);
-% Dg = e - G*z/lambda % gradient
-
-maxIter = 500;
+maxIter = 3000; % maybe excessive, but it gets precise results
 for j = 1:n*maxIter
     i = randi(n);
-    Dg(i) = 1 - G(i,:)*z/lambda;
-    %z(i) = z(i)+Dg(i)/sqrt(j);%
+    % set partial derivative = 0
+    z(i) = 0;
+    Dg(i) = 1 - G(i,:)*z/lambda; % partial without zi
     z(i) = Dg(i)*lambda/G(i,i);
+    % apply constraints
     if z(i) > 1
         z(i) = 1;
     elseif z(i) < 0
         z(i) = 0;
     end
 end
-%z = G\e % gradient = 0 (doesn't work, condition # too high?)
-zo = z;
 
+% Evaluate dual objective:
 D = sum(z) - (z'*G*z)/(2*lambda);
-w = YX'*z/lambda;
+%z = G\e % solve by gradient minimization (doesn't work, condition # too high?)
+
+% Convert from dual to primal variables
+w = (1/lambda)*(YX'*z);
+
+% Evaluate primal objective:
 P = sum(max(1-y.*(X*w),0)) + (lambda/2)*(w'*w);
-fprintf('Dual value: %2.4f\t Primal value: %3.4f\n',D,P)
+
+fprintf('Dual value: %2.4f\t Primal value: %3.4f\t %3.f support vectors\n',D,P,sum(z~=0))
