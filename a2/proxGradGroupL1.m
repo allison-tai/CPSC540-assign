1026d20bed7a8840b0502bd4f94f6352f978421a
sakukoivu <alistair.barton@mail.mcgill.ca>
2017-01-31 16:35:03 -0800

    NaN fixed

--- a2/proxGradGroupL1.m
+++ a2/proxGradGroupL1.m
@@ -1,4 +1,4 @@
-function [w,f] = proxGradL1(funObj,w,lambda,maxIter)
+function [w,f] = proxGradGroupL1(funObj,w,lambda,maxIter)
 % Minimize funOb(w) + lambda*sum(abs(w)) in groups % rows 0
 
 % Evaluate initial objective and gradient of smooth part
@@ -10,7 +10,7 @@ while funEvals < maxIter
     
     % proximal-gradient step
     alpha = 1/L;
-    w_new = softThreshold(w - alpha*g,alpha*lambda); % change this function for group
+    w_new = softThresholdg(w - alpha*g,alpha*lambda); % change this function for group
     [f_new,g_new] = funObj(w_new);  
     funEvals = funEvals + 1;
     
@@ -18,7 +18,7 @@ while funEvals < maxIter
     while f_new > f + g'*(w_new - w) + (L/2)*norm(w_new-w)^2
         L = L*2;
         alpha = 1/L;
-        w_new = softThreshold(w - alpha*g,alpha*lambda);
+        w_new = softThresholdg(w - alpha*g,alpha*lambda);
         [f_new,g_new] = funObj(w_new);
         funEvals = funEvals + 1;
     end
@@ -28,7 +28,7 @@ while funEvals < maxIter
     g = g_new;
 
     % Print out how we are doing
-    optCond = norm(w-softThreshold(w-g,lambda),'inf');
+    optCond = norm(w-softThresholdg(w-g,lambda),'inf');
     fprintf('%6d %15.5e %15.5e %15.5e\n',funEvals,alpha,f + lambda*sum(abs(w)),optCond);
     
     if optCond < 1e-1
@@ -37,11 +37,16 @@ while funEvals < maxIter
 end
 end
 
-function [w] = softThreshold(w,threshold) % change to group norm
+function [w] = softThresholdg(w,threshold) % change to group norm
     [d,k] = size(w);
     for i=1:d
-        groupnorm(i) = norm(w(i,:));
+        groupnorm_i = sqrt(sum(w(i,:).^2));
+        if groupnorm_i ~= 0
+            groupnorm(i) = groupnorm_i;
+        else
+            groupnorm(i) = 1;
+        end
     end
-    groupnorm = groupnorm'*ones(1,k);
+    groupnorm = groupnorm'*ones(1,k); 
     w = (w./groupnorm).*max(0,groupnorm-threshold);
 end
