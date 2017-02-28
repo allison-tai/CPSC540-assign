591f6d333800f65e1c0dc50073cf0ce6a0bd7c5a
sakukoivu <alistair.barton@mail.mcgill.ca>
2017-02-27 20:44:52 -0800

    donneennenene

--- a3/gaussian.m
+++ a3/gaussian.m
@@ -1,8 +1,8 @@
 function [L] = gaussian(xi,d,Sigma,mu,theta)
 % fits GDA to one row of data using parameters
 [n, d] = size(xi);
-lgSigma = logdet(Sigma);
-L = (2*pi)^(- d/2) *  lgSigma^(-0.5) * exp(-0.5 * ...
+detSigma = det(Sigma);
+L = (2*pi)^(- d/2) *  detSigma^(-0.5) * exp(-0.5 * ...
     (xi - mu)*(Sigma)^(-1)*(xi - mu)')*theta;
 end
 
