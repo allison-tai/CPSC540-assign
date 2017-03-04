6c57932a7fb5de7deaf7dc239c8c3029778c1eb7
sakukoivu <alistair.barton@mail.mcgill.ca>
2017-03-03 20:34:13 -0800

    1.1.1,2 done

---
+++ a4/marginalCK.m
@@ -0,0 +1,13 @@
+function [yOdds] = marginalCK(p0,pT)
+% Chapham Kolmogorov
+nNodes = size(pT,3)+1;
+yOdds = zeros(nNodes,1);
+
+nNodes = size(pT,3)+1;
+p = p0';
+yOdds(1) = p(2);
+for i=1:nNodes-1
+    p = pT(:,:,i)'*p;
+    yOdds(i+1) = p(2);
+end
+end
