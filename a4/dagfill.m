b7d29114298fb85ada74a4a5f5c5f79a17571d9d
sakukoivu <alistair.barton@mail.mcgill.ca>
2017-03-11 19:10:43 -0800

    2.3.2,3 markov+other

--- a4/dagfill.m
+++ a4/dagfill.m
@@ -2,21 +2,33 @@ clear all
 load MNIST_images.mat
 m = size(X,1);
 n = size(X,3);
+N = 3; M = N-1; % NxN DAG train
 
-% Train an inhomogeneous markov chain
+% Train an inhomogeneous markov chain on first three columns
 m_i = zeros(1,m); % starting distribution
-p_ij = zeros(m-2,m-2,2^8);
+p_ij = zeros(m-2,m-2,2);
+for j = 1:N
+    m_i(j) = sum(X(1,j,:))/n;
+    for i = 1:m-1
+        X0 = squeeze(X(i:i+1,j,:));
+        X1 = X0;
+        X0(:,X0(1,1,:)~=0) = [];
+        X1(:,X1(1,1,:)~=1) = [];
+        n1 = sum(X1(1,:));
+        p_ij(i,j,1) = sum(X0(2,:) == 1)/max(n-n1,1);
+        p_ij(i,j,2) = sum(X1(2,:) == 1)/max(n1,1);
+    end
+end
+% train NxN DAG on remainder
 models = cell(m,m,4);
-X = sparse(X);
-for j = 1:m-2
-    j
-    for i = 1:m-2
-        X0 = X(i:i+2,j:j+2,:);
-        X0 = reshape(X0(:,:,:),9,n)';
-        Xij = X0(:,1:8);
-        yij = X0(:,9);
+for j = 1:m-M
+    for i = 1:m-M
+        X0 = X(i:i+M,j:j+M,:);
+        X0 = reshape(X0(:,:,:),N^2,n)';
+        Xij = X0(:,1:N^2-1);
+        yij = X0(:,N^2);
         modelij = binaryTabular(Xij,yij,1);
-        models(i+2,j+2,:) = struct2cell(modelij);
+        models(i+M,j+M,:) = struct2cell(modelij);
     end
 end
 
@@ -34,12 +46,12 @@ for image = 1:4
     for i = 1:m
         for j = 1:m
             if isnan(I(i,j))
-                if j < 3
-                    I(i,j) = 0;
+                if j < N
+                    I(i,j) = rand < p_ij(i-1,j,I(i-1,j)+1);
                 else
-                    Zhat = I(i-2:i,j-2:j);
-                    Zhat = reshape(Zhat(:,:),9,1)';
-                    Xhat = Zhat(1:8);
+                    Zhat = I(i-M:i,j-M:j);
+                    Zhat = reshape(Zhat(:,:),N^2,1)';
+                    Xhat = Zhat(1:N^2-1);
                     model = cell2struct(models(i,j,:),...
                         {'rows','counts','sample','alpha'},3);
                     I(i,j) = model.sample(model,Xhat);
