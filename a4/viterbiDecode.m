d2e755d6ae6f46e61e1d444d85ff6769cfb81b44
l0f8 <allison.tai@alumni.ubc.ca>
2017-03-24 14:47:08 -0700

    Add 1.2.4

--- a4/viterbiDecode.m
+++ a4/viterbiDecode.m
@@ -4,14 +4,16 @@ d = size(pT,3)+1;
 M = zeros(2,d);
 T = zeros(2,d);
 
-M(:,1) = p0(1);
+M(:,1) = p0;
 
+% memoize
 for i=1:d-1
     for s=1:2
         m = [M(1,i)*pT(1,s,i),M(2,i)*pT(2,s,i)];
         [M(s,i+1), T(s,i+1)] = max(m);
     end
 end
+
 y_best = zeros(d,1);
 [mx, argmx] = max(M(:,d));
 y_best(d,1) = argmx;
