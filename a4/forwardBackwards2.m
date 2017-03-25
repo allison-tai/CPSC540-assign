bd39b7f4d171b502aafee755c86010a6970b3a8c
sakukoivu <alistair.barton@mail.mcgill.ca>
2017-03-25 16:53:56 -0700

    edit of 1.2 stuff

---
+++ a4/forwardBackwards2.m
@@ -0,0 +1,32 @@
+function [p] = forwardBackwards2(p0,pT)
+
+% Set up our DP table
+d = size(pT,3)+1;
+V = forwards(p0,pT,d);
+B = backwards(pT, d);
+p = zeros(d,2);
+
+for j=1:d
+   p(j,:) = V(j,:).*B(j,:);
+   % Normalize
+   p(j,:) = p(j,:)/sum(p(j,:),2);
+end
+end
+
+function [M] = forwards(p0,pT,d)
+M = zeros(d,2);
+M(1,:) = p0';
+for j=2:d
+    M(j,1) = M(j-1,1)*pT(1,1,j-1) + M(j-1,2)*pT(2,1,j-1);
+    M(j,2) = M(j-1,1)*pT(1,2,j-1) + M(j-1,2)*pT(2,2,j-1);
+end 
+end
+
+function [M] = backwards(pT,d)
+M = ones(d,2);
+M(d,:) = [1 0];
+for j=d-1:-1:1
+    M(j,1) = M(j+1,1)*pT(1,1,j) + M(j+1,2)*pT(1,2,j);
+    M(j,2) = M(j+1,1)*pT(2,1,j) + M(j+1,2)*pT(2,2,j);
+end
+end
---
+++ a4/sampleBackwardsMC.m
@@ -0,0 +1,28 @@
+function [yOdds] = sampleBackwards(pd,pT)
+
+% Set up our DP table
+d = size(pT,3)+1;
+% number of samples
+n = 10000;
+
+% fill in DP table
+y = zeros(n,d);
+y(:,d) = rand(n,1) > pd(1)*ones(n,1);
+% sample from our distribution, backwards (we know there is 0 chance of
+% being in state 2)
+for j=d-1:-1:1
+    yp = pT(:,y(:,j+1)+1,j)';
+    % normalize
+    yp = yp./(sum(yp,2)*ones(1,2));
+    %samples
+    y(:,j) = rand(n,1) > yp(:,1);
+end
+yOdds = mean(y)';
+
+% sample from our distribution, backwards (we know there is 0 chance of
+% being in state 2)
+%y = zeros(n,d);
+%for j=d-1:-1:1
+%        y(:,j) = rand(n,1) > M(j,y(:,j+1)+1)';
+%end
+end
