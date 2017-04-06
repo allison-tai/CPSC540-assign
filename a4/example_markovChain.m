24a1d3b33f570107428384b8f98bec9855924f9f
l0f8 <allison.tai@alumni.ubc.ca>
2017-04-06 10:04:22 -0700

    Mark updated example_UGM

--- a4/example_markovChain.m
+++ a4/example_markovChain.m
@@ -3,8 +3,8 @@ clear all
 load viterbiData.mat
 
 decode_short1 = exactDecode(p0,pT_short1)
-decode_short1 = sampleAncestral(p0,pT_short1)
+decode_short1 = sampleBackwards_allison(p0,pT_long)
 
-decode_short2 = exactDecode(p0,pT_short2)
+decode_short2 = sampleBackwardsMC([1 0],pT_long)
 
 %decode_long = exactDecode(p0,pT_long)
