3fe2c6cee923a4993fb286d87eddd2a9b1bcc8b6
sakukoivu <alistair.barton@mail.mcgill.ca>
2017-04-06 14:39:03 -0700

    old stuff

--- a4/sampleBackwards2.m
+++ a4/sampleBackwards2.m
@@ -1,21 +1,24 @@
-function [yOdds] = sampleBackwards(pd,pT)
+function [yOdds] = sampleBackwards2(pd,pT)
 
 % Set up our DP table
 d = size(pT,3)+1;
+V = zeros(d,2);
+
 % number of samples
 n = 10000;
 
 % fill in DP table
+V(end,:) = pd;
 y = zeros(n,d);
-y(:,d) = rand(n,1) > pd(1)*ones(n,1);
+y(:,d) = rand(n,1) > V(d,1);
 % sample from our distribution, backwards (we know there is 0 chance of
 % being in state 2)
 for j=d-1:-1:1
-    yp = pT(:,y(:,j+1)+1,j)';
+    V(j,:) = pT(:,:,j)*V(j+1,:)';
     % normalize
-    yp = yp./(sum(yp,2)*ones(1,2));
+    V(j,:) = V(j,:)/sum(V(j,:));
     %samples
-    y(:,j) = rand(n,1) > yp(:,1);
+    y(:,j) = rand(n,1) > V(j,1);
 end
 yOdds = mean(y)';
 
