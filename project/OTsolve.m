3be9fd21cae24ceadccb91541841ca96e9d75a02
sakukoivu <alistair.barton@mail.mcgill.ca>
2017-04-14 03:36:33 -0700

    solves regularized tranport

---
+++ project/OTsolve.m
@@ -0,0 +1,28 @@
+function [C gamma] = OTsolve(cost,x,y,tol,lambda)
+    %OTSOLVE Solves for the regularized optimal transport between vectors x and
+    %multiple vectors contained in y.
+    
+    n = length(x);
+
+    % apply sk from negentropy paper
+    I = (x>0);
+    x = x(I); cost = cost(I,:); K = exp(-lambda*cost);
+    u = ones(length(x),size(y,2))/length(x);
+    Ktilde = bsxfun(@rdivide,cost,x);
+    uold = Inf;
+    while norm(u-uold)>tol
+        uold = u;
+        u = 1./(Ktilde*(y./(K'*u)));
+    end
+    v = y./(K'*u);
+    C = sum(u.*((K.*cost)*v));
+
+    % construct gamma (diffusion)
+    gamma = zeros(n,size(y,1),size(y,2));
+    for i = 1:size(y,2)
+        du = diag(u(:,i));
+        dv = diag(v(:,i));
+        gamma(find(I),:,i) = du*K*dv; % includes empty indices
+    end
+end
+
