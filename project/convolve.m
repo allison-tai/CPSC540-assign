db85df7d09c5f882a26b3912f361705d36f20699
sakukoivu <alistair.barton@mail.mcgill.ca>
2017-04-14 06:21:00 -0700

    added some basic convolutions

---
+++ project/convolve.m
@@ -0,0 +1,18 @@
+function [ imgfilt ] = convolve(img,filter,stride)
+%CONVOLVE Summary of this function goes here
+%   Detailed explanation goes here
+    [m n] = size(filter);
+    [M N] = size(img);
+    mu = floor(M-m+1/stride);
+    nu = floor(N-n+1/stride);
+    imgfilt = zeros(mu,nu);
+    for i = 1:mu
+        for j = 1:nu
+            imgfilt(i,j) = sum(sum(filter.*img(i:i+m-1,j:j+n-1)));
+        end
+    end
+    % make everything positive
+    %imgfilt =abs(imgfilt)/max(max(abs(imgfilt))); 
+    imgfilt =(imgfilt+min(min(imgfilt)))/max(max(imgfilt)); 
+end
+
