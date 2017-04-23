6c98a3a48802d80d6590c5c64e212eebac8df447
l0f8 <allison.tai@alumni.ubc.ca>
2017-04-23 12:05:23 -0700

    Built the pixel shift

--- project/OTsolve.m
+++ project/OTsolve.m
@@ -5,10 +5,15 @@ function [C gamma] = OTsolve(cost,x,y,tol,lambda)
     n = length(x);
     % normalize
     x = x/sum(x);
-    y = y./sum(y);
+    stuff = ones(size(y,1),1)*sum(y); 
+    y = y./stuff;
 
     % apply sk from negentropy paper
     I = (x>0);
+    % I assume x tells me which pixels are lit up
+    % I only look at the transportation of white pixels, hence cost(I,:)
+    % I need to know the optimal destination of each pixel
+    % Or just which direction each pixel moved
     x = x(I); cost = cost(I,:); K = exp(-lambda*cost);
     u = ones(length(x),size(y,2))/length(x);
     Ktilde = bsxfun(@rdivide,cost,x);
@@ -18,6 +23,8 @@ function [C gamma] = OTsolve(cost,x,y,tol,lambda)
         u = 1./(Ktilde*(y./(K'*u)));
     end
     v = y./(K'*u);
+    % without the sum, I get the cost of transporting each white pixel of
+    % x, but no idea where the destination of that pixel is
     C = sum(u.*((K.*cost)*v));
 
     % construct gamma (diffusion)
---
+++ project/pixelTrans.m
@@ -0,0 +1,39 @@
+function [xnew] = pixelTrans(gamma, x, n)k
+% translates pixels of x by average move
+
+[g1,g2,g3] = size(gamma);
+
+distX = zeros(g1, g3);
+distY = zeros(g1, g3);
+    
+for i = 1:g1
+    % look through each pixel
+    currX = mod(i,n);
+    currY = ceil(i/n);
+        
+    for j = 1:g3
+        % look through each test set
+        % get the 28 x 28 thing
+        distribution = reshape(gamma(i,:,j), [n n]);
+        % get average point (expectation)
+        bestX = round(sum((1:n).*(mean(distribution, 1)./sum(mean(distribution, 1)))));
+        bestY = round(sum((1:n).*(mean(distribution, 2)./sum(mean(distribution, 2)))'));
+        % now find how many pixels vertically/horizontally we need to move
+        distX(i,j) = bestX - currX;
+        distY(i,j) = bestY - currY;
+    end
+end
+
+distX(isnan(distX)) = 0;
+distY(isnan(distY)) = 0;
+% mean move of all pixels (we'll shift every pixel in the image by this
+% much
+moveX = round(mean((mean(distX))~=0));
+moveY = round(mean((mean(distY))~=0));
+% now shift
+xsmall = reshape(x, [n n]);
+xnew = zeros(n, n);
+xnew(moveX+1:end,moveY+1:end) = xsmall(1:end-moveX,1:end-moveY);
+xnew = reshape(xnew, [g1 1]);
+
+end
