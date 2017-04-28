440218e31aff262fd8d74f181c4f9075ff6d8114
sakukoivu <alistair.barton@mail.mcgill.ca>
2017-04-28 04:39:14 -0700

    CNN-commented

--- project/convolve.m
+++ project/convolve.m
@@ -1,17 +1,20 @@
 function [ imgfilt ] = convolve(img,filter,stride)
 %CONVOLVE Convolves img with filter, with stride
+    img = reshape(img,[28 28]);
     [m n] = size(filter);
     [M N] = size(img);
-    mu = floor(M-m+1/stride);
-    nu = floor(N-n+1/stride);
-    imgfilt = zeros(mu,nu);
-    for i = 1:mu
-        for j = 1:nu
-            imgfilt(i,j) = sum(sum(filter.*img(i:i+m-1,j:j+n-1)));
+    imgfilt = zeros(M,N);
+    newimage = zeros(30,30); % add zeros to edge of image
+    newimage(2:29,2:29) = img;
+    img = newimage;
+    for i = 2:M+1
+        for j = 2:M+1
+            imgfilt(i,j) = sum(sum(filter.*img(i-1:i+m-2,j-1:j+n-2)));
         end
     end
     % make everything positive
     %imgfilt =abs(imgfilt)/max(max(abs(imgfilt))); 
     imgfilt =(imgfilt+min(min(imgfilt)))/max(max(imgfilt)); 
+    imgfilt = reshape(imgfilt(2:29,2:29),[28^2 1]);
 end
 
